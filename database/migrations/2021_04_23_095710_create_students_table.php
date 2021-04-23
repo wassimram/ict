2e75e71a0c3110ee46abe77178c2627fa8b8704f
Dr. Wassim Ramadan <wassimram@gmail.com>
2021-04-23 13:54:51 +0300

    changing Student table. adding lname and fname columns

--- database/migrations/2021_04_23_095710_create_students_table.php
+++ database/migrations/2021_04_23_095710_create_students_table.php
@@ -15,6 +15,8 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('fname');
+            $table->string('lname');
             $table->timestamps();
         });
     }
